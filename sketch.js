e9624714921b1cc4aa67ffdc58062c95cd177e8f
Sweekarplayz <70138621+Sweekarpp@users.noreply.github.com>
2020-12-17 10:12:34 +0530

    lol

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
-  var dog,database,foodS,FoodStock,dogImg,dogImg1,dog2;
-  function preload(){
-    dogImg = loadImage('images/dogImg.png');
-    dogImg1 = loadImage('images/dogImg1.png')
-  }
+var dog,database,foodS,FoodStock,dogImg,dogImg1,dog2;
+function preload(){
+  dogImg = loadImage('images/dogImg.png');
+  dogImg1 = loadImage('images/dogImg1.png')
+}
 
-  function setup(){
-    createCanvas(500, 500);
-    database = firebase.database();
-    dog = createSprite(250,250,200,200);
-    dog.addImage("dog",dogImg);
-    dog.scale = 0.2;
+function setup(){
+  createCanvas(500, 500);
+  database = firebase.database();
+  dog = createSprite(250,250,200,200);
+  dog.addImage("dog",dogImg);
+  dog.scale = 0.2;
 
-    FoodStock = database.ref('Food');
-    FoodStock.on("value",readFood);
-  }
+  FoodStock = database.ref('Food');
+  FoodStock.on("value",readFood);
+}
 
 
-  function draw(){
+function draw(){
 background(46,139,87);
 if(keyWentDown(UP_ARROW)){
 writeStock(foodS);
@@ -25,26 +25,26 @@ dog2 = createSprite(250,250,200,200);
 dog2.addImage(dogImg1);
 dog2.scale = 0.2;
 }
-    drawSprites();
-    //add styles here
+  drawSprites();
+  //add styles here
 
-  }
+}
 
-  function readFood(data){
-    foodS = data.val();
+function readFood(data){
+  foodS = data.val();
 
 }
 
 function writeStock(x){
 
 if(x<=0){
-  x=0;
+x=0;
 
 }else{
-  x=x-1
+x=x-1
 }
 
-  database.ref('/').update({
+database.ref('/').update({
 Food:x
-  })
+})
 }
